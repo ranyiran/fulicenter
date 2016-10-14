d6bd5a45c8b8b0dae328d81d0153c691ba204eb6
ranyiran <271536025@qq.com>
2016-10-14 15:37:00 +0800

    add AppLogo from AndroidManifest,make Splash Background，use db and New thread to Splash，finish Splash;

---
+++ app/src/main/java/cn/ran/flicenter/SplashActivity.java
@@ -0,0 +1,47 @@
+package cn.ran.flicenter;
+
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private final long sleepTime = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                long start = System.currentTimeMillis();
+                //creatd db执行耗时操作
+                long costTime = System.currentTimeMillis() - start;
+                if (sleepTime - costTime > 0) {
+                    try {
+                        Thread.sleep(sleepTime - costTime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+
+            }
+        }).start();
+
+    }
+}
